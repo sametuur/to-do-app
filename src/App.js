553c1f49efc7c35b781be29385b5cf47b61f7762
sametuur <sametugur.aslan@hotmail.com>
2022-11-30 22:23:04 +0300

    popup get data.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
-import Popup   from "./components/modals/Popup";
-
+import Popup from "./components/modals/Popup";
+import "./App.css";
 
 function App() {
   const [Modelopen, setModelOpen] = useState(false);
 
   return (
-   <div>
-    <button onClick={() => setModelOpen(true)}> Click to Open Popup</button>
-    <Popup  open={Modelopen} close={()=> setModelOpen(false)} />
-    {/* <div>{Popup.data}</div> */}
-   </div>
- );
-};
+    <div className="all-div">
+      <button className="button-popup" onClick={() => setModelOpen(true)}>
+        Click to Open Popup
+      </button>
+      <Popup open={Modelopen} close={() => setModelOpen(false)} />
+      {/* <div>{Popup.data}</div> */}
+    </div>
+  );
+}
 export default App;
--- src/components/modals/Popup.js
+++ src/components/modals/Popup.js
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
+import "react-bootstrap";
 import "./popup.css";
 
-function Popup({open,close}) {
-  const data="samet";
+function Popup({ open, close }) {
+  const [data, setData] = useState();
   console.log(data);
-  if(!open) return null;
+
+  const Change = (event) => {
+    if (event.key === 'Enter')
+    setData(event.target.value);
+  };
+
+  if (!open) return null;
   return (
-    <><p onClick={close}>popup</p>
-     <div>
-      <form>
-        <input type="text" placeholder="task add "></input>
-      </form>
-      </div></>
-    
+    <>
+      <div className="popup-container">
+        <div className="popup-body">
+          <form>
+            <p onClick={close}>x</p>
+            <input
+              value={data}
+              onClick={Change}
+              //page refresh when enter
+              onKeyDown={Change}
+              placeholder="task add "
+            ></input>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
-export default Popup
+export default Popup;
